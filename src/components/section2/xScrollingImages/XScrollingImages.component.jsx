4da9ebdc94a8d972775ace441d2482a5b2388ae4
Itay Leybovich <Itayleybovich@gmail.com>
2022-02-17 20:15:05 +0200

    V1.1 - bug fixes

--- src/components/section2/xScrollingImages/XScrollingImages.component.jsx
+++ src/components/section2/xScrollingImages/XScrollingImages.component.jsx
@@ -1,24 +1,26 @@
+import ReactDOM from 'react-dom';
 import React, { useEffect, useRef } from 'react';
 import ScrollingContainer from 'react-indiana-drag-scroll';
 import './xScrollingImages.styles.scss';
 
 const XScrollingImages = () => {
     const scrollDiv = useRef();
-    
-    useEffect(()=>{
-        if(scrollDiv.current) {
-            setTimeout(()=>{
-                scrollDiv.current.scrollTo(450,0);
-            },10)
-        }
-    },[])
+    const scroll = () => {
+        scrollDiv.current.getElement().scrollTo(450,0)
+    }
     
     return (
-        <div className='x-scrolling-images-container' >
-            <ScrollingContainer className='x-scrolling-images' vertical={false} innerRef={scrollDiv}>
-                <img src="/assets/jpgs/divers_left.jpg" alt="divers_left" draggable="false" onDragStart={()=>{return false;}}/>
-                <img src="/assets/jpgs/divers_middle.jpg" alt="divers_middle" draggable="false" onDragStart={()=>{return false;}}/>
-                <img src="/assets/jpgs/divers_right.jpg" alt="divers_right" draggable="false" onDragStart={()=>{return false;}}/>
+        <div className='x-scrolling-images-container'>
+            <ScrollingContainer className='x-scrolling-images' vertical={false} ref={scrollDiv}>
+                <div className='image-container'>
+                    <img src="/assets/jpgs/divers_left.jpg" alt="divers_left"/>
+                </div>
+                <div className='image-container'>
+                    <img src="/assets/jpgs/divers_middle.jpg" alt="divers_middle"/>
+                </div>
+                <div className='image-container'>
+                    <img src="/assets/jpgs/divers_right.jpg" alt="divers_right" onLoad={()=>{scroll()}}/>
+                </div>
             </ScrollingContainer>
         </div>   
     )
